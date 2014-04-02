d32921058c0b85e22fbaf4a6033181b581e100e3
Elliot Wolk <elliot.wolk@gmail.com>
2014-04-02 15:42:01 -0400

    compile: remove ffmpeg compilation, bundled libav works nicely

--- compile.pl
+++ compile.pl
@@ -2,32 +2,18 @@
 use strict;
 use warnings;
 
-sub ffmpegCompile();
 sub run(@);
 
 my $repoDir = "$ENV{HOME}/Code/stepmania";
 my $installDir = "$ENV{HOME}/Desktop/Games/stepmania";
 
-my $ffmpegUrl = "http://ffmpeg.org/releases";
-my $ffmpegVersion = "0.10.2";
 my $threads = 8;
-my @ffmpegFlags = qw(
-  --enable-static --enable-gpl --enable-version3
-  --enable-nonfree --enable-libx264 --enable-libfaac
-  --enable-libmp3lame --enable-libtheora --enable-libvorbis
-  --disable-libvpx --disable-vaapi --enable-libxvid --disable-debug
-  --enable-memalign-hack --disable-network --enable-small --disable-encoders
-  --disable-ffserver --extra-cflags=-Dattribute_deprecated=
-);
 
 sub main(@){
   chdir $repoDir;
   run "./autogen.sh";
 
-  ffmpegCompile();
-
-  run "./configure", "--with-ffmpeg",
-    "--with-static-ffmpeg=./ffmpeg-$ffmpegVersion/_inst";
+  run "./configure", "--with-ffmpeg";
 
   run "make", "-j$threads";
 
@@ -40,8 +26,6 @@ sub install(){
   my @excludes = map {"--exclude=$_"} qw(
     .git/
     _assets/ extern/ Program/ Utils/
-    ffmpeg-*/
-    ffmpeg-*.tar.bz2
     Songs/ Themes/ Xcode/
   );
   run "mkdir", "-p", $installDir;
@@ -49,27 +33,6 @@ sub install(){
   run "mkdir", "-p", "$buildDir/Songs";
   run "rm", "-f", "$installDir/latest";
   run "ln", "-s", $buildName, "$installDir/latest";
-}
-
-sub ffmpegCompile(){
-  my $name = "ffmpeg-$ffmpegVersion";
-  my $instDir = "$name/_inst";
-  my $tar = "$name.tar.bz2";
-
-  if(-d $instDir){
-    print "skipping compiled ffmpeg\n";
-    return;
-  }
-  run "wget", "$ffmpegUrl/$tar" if not -e $tar;
-  run "rm", "-rf", "$name/";
-  run "tar", "-xf", $tar;
-
-  die "no $name/ dir\n" if not -d $name;
-  chdir $name;
-
-  run "./configure", "--prefix=\"./_inst\"", @ffmpegFlags;
-  run "make", "-j$threads", "install-libs", "install-headers";
-  chdir "../";
 }
 
 sub run(@){
